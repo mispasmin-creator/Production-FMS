29e441bb98854d6e64050f119ec27c8c41ef9d50
dverma9165 <dverma9165@gmail.com>
2026-05-23 14:08:24 +0530

    fix css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-// import "./globals.css";
+import "./globals.css";
 import { AuthProvider } from "@/lib/auth";
 import { AppShell } from "@/components/AppShell";
 
